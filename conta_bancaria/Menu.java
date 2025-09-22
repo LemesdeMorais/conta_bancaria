b5f873c3aae295fff1a5813e37c16dc7f0e1b7af
Rafaela Lemes <r.lemesdemorais@gmail.com>
2025-09-22 10:19:59 -0300

    Versão Menu

--- conta_bancaria/Menu.java
+++ conta_bancaria/Menu.java
@@ -1,10 +1,96 @@
 package conta_bancaria;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+		
+		int op=0;
+		
+		//while(true) { // verificar onde tenho que fechar essa chave
+		//System.out.println("****************************************************************************");
+		System.out.println("");
+		System.out.println("\t\t\t Gringotes o Banco das Bruxas");
+		System.out.println("");
+		System.out.println("****************************************************************************");
+		System.out.println("");
+		System.out.println("\t\t\t 1 - Criar Conta");
+		System.out.println("\t\t\t 2 - Listar todas as Contas");
+		System.out.println("\t\t\t 3 - Buscar Conta por Numero");
+		System.out.println("\t\t\t 4 - Atualizar Dados da Conta");
+		System.out.println("\t\t\t 5 - Apagar Conta");
+		System.out.println("\t\t\t 6 - Sacar");
+		System.out.println("\t\t\t 7 - Depositar");
+		System.out.println("\t\t\t 8 - Transferir valores entre Contas");
+		System.out.println("\t\t\t 0 - Sair");
+		System.out.println("");
+		System.out.println("****************************************************************************");
+		System.out.println("Entre com a opção desejada:");
+		
+		try {
+		op = leia.nextInt();
 
-	}
+		if (op == 0) {
+			System.out.println("\n Gringotes o Banco das Bruxas - O lugar mais seguro!");
+			leia.close();
+			System.exit(0);
+			}else if(op >= 9 ) {
+				System.out.println("\n Opção inválida! Tente novamente.");
+				}
+		}catch (InputMismatchException e) {
+			System.out.println("ERRO! Entrada inválida. Tente novamente.");
+			leia.next();
+		}
 
+		
+		
+		switch (op) {
+			case 1: System.out.println("Criar Conta");
+			break;
+			
+			case 2: System.out.println("Listar todas as contas");
+			break;
+			
+			case 3: System.out.println("Buscar Conta por Numero");
+			break;
+			
+			case 4: System.out.println(" Atualizar Dados da Conta");
+			break;
+			
+			case 5: System.out.println("Apagar Conta");
+			break;
+			
+			case 6: System.out.println("Sacar");
+			break;
+			
+			case 7: System.out.println("Depositar");
+			break;
+			
+			case 8: System.out.println("Transferir valores entre Contas");
+			break;
+		}
+		
+		
+		sobre();
+	
+		//}
+	
 }
+
+	public static void sobre(){
+		System.out.println("\n****************************************************************************"
+				+ 			"\n\tProjeto deselvoldito por:\n"
+				+ 			"\tRafaela Lemes - Dev Full Stack\n "
+				+ 			"\tgithub.com/lemesdemorais\n"
+				+ 			"\n****************************************************************************");		
+	}
+}
+
+	
+	
+		
+	
+
